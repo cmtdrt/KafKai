bc23b6eb3dec8d63c0737f1b4330fb62e86d7e2a
Clément DROUET <drouet.clement02@gmail.com>
2026-03-01 11:44:27 +0100

    get consumer args

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,4 +1,32 @@
+use common::{Offset, DEFAULT_BIND_ADDR};
+
 fn main() {
-    println!("Init consumer");
+    let mut args = std::env::args().skip(1);
+
+    let topic = match args.next() {
+        Some(t) => t,
+        None => {
+            eprintln!("Usage: consumer <topic> <offset> [max_messages]");
+            std::process::exit(1);
+        }
+    };
+
+    let offset: Offset = match args.next() {
+        Some(o) => o.parse().map_err(|e| format!("Invalid offset: {}", e)).unwrap(),
+        None => {
+            eprintln!("Usage: consumer <topic> <offset> [max_messages]");
+            std::process::exit(1);
+        }
+    };
+
+    let max_messages: usize = match args.next() {
+        Some(m) => m
+            .parse()
+            .map_err(|e| format!("Invalid max_messages: {}", e)).unwrap(),
+        None => 10,
+    };
+
+    println!("topic: {}, offset: {}, max_messages: {}", topic, offset, max_messages);
+    println!("Connecting to broker at {}", DEFAULT_BIND_ADDR);
 }
 
